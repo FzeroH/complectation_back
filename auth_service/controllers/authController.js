275d99d088ece3c2b1fd48e07ba5362f951941c3
Александр <vampir666x@gmail.com>
2023-05-05 15:26:46 +0300

    Добавил функции login и registration

--- auth_service/controllers/authController.js
+++ auth_service/controllers/authController.js
@@ -1,45 +1,62 @@
-const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 const { db } = require('../configs/postgresConfig');
 
 
 module.exports.login = async function (req, res){
-    const request = req.body
-    await db.query()
+    const { email, password } = req.body;
+
+    try {
+        const user = await db.oneOrNone('SELECT * FROM users WHERE user_email = ${email}', { email: email });
+
+        if (!user) {
+            return res.status(401).json({ error: 'Неверный email или пароль' });
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if (!passwordMatch) {
+            return res.status(401).json({ error: 'Неверный email или пароль' });
+        }
+
+        const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+        res.json({ token, user: { id: user.id, email: user.email } });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Произошла ошибка' });
+    }
 };
 
 module.exports.registration =  async function (req, res) {
-    const request = req.body
-    const candidate = await db.query(`SELECT email FROM user WHERE user_email = ${ email }`,{
-        email: request.email
-    })
-    if(candidate){
-        res.status(404).json({
-            message:'Пользователь с таким e-mail уже существует'
-        })
-    } else {
-        bcrypt.genSalt(10, function (err, salt) {
-            bcrypt.hash(request.password, salt, function (err, hash) {
-                db.query(`INSERT INTO user (first_name, last_name, email, password))
-                          VALUES (${first_name}, ${last_name}, ${email}, ${password})`,
-                    {
-                        first_name: request.first_name,
-                        last_name: request.last_name,
-                        email: request.email,
-                        password: hash
-                    })
-                    .then(() => {
-                        res.status(200).json({
-                            message: 'Успешно'
-                        })
-                    })
-                    .catch(e => {
-                        res.status(401).json({
-                            message: e.message
-                        })
-                    })
-                console.log(hash);
-            });
+    const { user_first_name, user_last_name, user_email, user_password } = req.body;
+    try {
+
+        const candidate = await db.oneOrNone('SELECT user_id FROM users WHERE user_email = ${email}', {
+            email: user_email
         });
+
+        if (candidate) {
+            return res.status(400).json({ error: 'Пользователь уже зарегистрирован' });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = await db.one(
+            'INSERT INTO users (user_first_name, user_last_name, user_email, user_password) VALUES (${first_name}, ${last_name}, ${email},${password})',
+            {
+                first_name: user_first_name,
+                last_name: user_last_name,
+                email: user_email,
+                password: hashedPassword
+            });
+
+
+        const token = jwt.sign({ userId: newUser.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+        res.json({ token, user: { id: newUser.id, email: newUser.email } });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Произошла ошибка' });
     }
 };
 
