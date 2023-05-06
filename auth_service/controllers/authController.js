08fd256fa2a03e74f1a94817cb84cf1065f02cd2
Александр <vampir666x@gmail.com>
2023-05-06 14:12:57 +0300

    Подправил названия переменных. Убрал генерацию токена при регистрации

--- auth_service/controllers/authController.js
+++ auth_service/controllers/authController.js
@@ -4,23 +4,23 @@ const { db } = require('../configs/postgresConfig');
 
 
 module.exports.login = async function (req, res){
-    const { email, password } = req.body;
+    const { users_email, users_password } = req.body;
 
     try {
-        const user = await db.oneOrNone('SELECT * FROM users WHERE user_email = ${email}', { email: email });
+        const user = await db.oneOrNone('SELECT * FROM users WHERE user_email = ${email}', { email: users_email });
 
         if (!user) {
             return res.status(401).json({ error: 'Неверный email или пароль' });
         }
 
-        const passwordMatch = await bcrypt.compare(password, user.password);
+        const passwordMatch = await bcrypt.compare(users_password, user.users_password);
         if (!passwordMatch) {
             return res.status(401).json({ error: 'Неверный email или пароль' });
         }
 
-        const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        const token = jwt.sign({ userId: user.users_id, userRole: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
-        res.json({ token, user: { id: user.id, email: user.email } });
+        res.json({ token, user: { id: user.users_id, email: user.users_email } });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Произошла ошибка' });
@@ -28,11 +28,11 @@ module.exports.login = async function (req, res){
 };
 
 module.exports.registration =  async function (req, res) {
-    const { user_first_name, user_last_name, user_email, user_password } = req.body;
+    const { users_first_name, users_last_name, users_email, users_password } = req.body;
     try {
 
-        const candidate = await db.oneOrNone('SELECT user_id FROM users WHERE user_email = ${email}', {
-            email: user_email
+        const candidate = await db.oneOrNone('SELECT users_id FROM users WHERE users_email = ${ email }', {
+            users_email: users_email,
         });
 
         if (candidate) {
@@ -44,16 +44,12 @@ module.exports.registration =  async function (req, res) {
         const newUser = await db.one(
             'INSERT INTO users (user_first_name, user_last_name, user_email, user_password) VALUES (${first_name}, ${last_name}, ${email},${password})',
             {
-                first_name: user_first_name,
-                last_name: user_last_name,
-                email: user_email,
+                first_name: users_first_name,
+                last_name: users_last_name,
+                email: users_email,
                 password: hashedPassword
             });
 
-
-        const token = jwt.sign({ userId: newUser.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-
-        res.json({ token, user: { id: newUser.id, email: newUser.email } });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Произошла ошибка' });
@@ -62,7 +58,13 @@ module.exports.registration =  async function (req, res) {
 
 module.exports.getUserData = async function (req, res){
     const request = req.body
-    await db.query('SELECT * FROM user WHERE user_id = ${ user_id }', {
+    const result = await db.one('SELECT * FROM user WHERE user_id = ${ user_id }', {
         user_id:request.user_id
+    })
+    return res.status(200).json({
+        users_id: result.users_id,
+        users_first_name: result.users_id,
+        users_last_name: result.users_id,
+        users_role: result.users_role,
     })
 };
