bf7ebb7adc629417131a20c2685e6637052f59d5
Александр <vampir666x@gmail.com>
2023-05-07 19:23:05 +0300

    Изменил название функции и убрал проверку на статус админа

--- admin_service/middleware/authenticateToken.js
+++ admin_service/middleware/authenticateToken.js
@@ -1,16 +1,12 @@
 const jwt = require("jsonwebtoken");
 
-module.exports.authenticateAdminToken = async function (req, res, next) {
+module.exports.authenticateToken = async function (req, res, next) {
     const token = req.headers['authorization'];
     if (!token) {
         return res.status(401).send('JWT токен не найден');
     }
     try {
         const decoded = jwt.verify(token, 'secret_key');
-        //TODO:Подумать о том, как лучше осуществлять проверку роли пользователя
-        if (decoded.role_id !== 'administartor') {
-            return res.status(401).send('Доступ запрещен. Пользователь не является администратором');
-        }
         req.user = decoded;
         next();
     } catch (err) {
