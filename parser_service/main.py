e71d8b560e72473f4a6d38f97de478cd63c53eed
Александр <vampir666x@gmail.com>
2023-05-07 19:20:48 +0300

    Подключил проверку JWT токена

--- parser_service/main.py
+++ parser_service/main.py
@@ -1,19 +1,27 @@
 import os
-from fastapi import FastAPI, File, UploadFile, Form
+from fastapi import FastAPI, File, UploadFile, Form, Depends, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from fastapi.responses import JSONResponse
 import pandas as pd
 
+from verifyToken import verify_token
 from parser import parser
 
 app = FastAPI()
+security = HTTPBearer()
 
-@app.get("/{filename}/{id}")
-async def test(filename, id):
-    test = parser(f"{filename}")
-    return {"message": test[int(id)]}
 
 @app.post("/api/file/upload")
-async def create_upload_file(file: UploadFile = File(...), publisher: str = Form(...)):
+async def create_upload_file(
+        file: UploadFile = File(...),
+        publisher: str = Form(...),
+        credentials: HTTPAuthorizationCredentials = Depends(security)):
+    try:
+        payload = verify_token(credentials.credentials)
+        user_id = await payload.get("userId")
+    except Exception as e:
+        raise HTTPException(status_code=401, detail="Токен недействителен или истекло время действия токена.")
+
     if not file.filename.lower().endswith(('.xls', '.xlsx')):
         return JSONResponse(content={"error": "Неверный формат файла."}, status_code=400)
 
@@ -34,4 +42,3 @@ async def create_upload_file(file: UploadFile = File(...), publisher: str = Form
     os.remove(file_path)
 
     return {"filename": file.filename, "publisher": publisher, "test": t}
-
