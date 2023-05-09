3d7e97e36facbb50c36ca897f1f380e7c8814d64
Александр <vampir666x@gmail.com>
2023-05-09 19:55:30 +0300

    Добавил функцию проверки роли администратора у пользователя

---
+++ admin_service/middleware/authenticateAdmin.js
@@ -0,0 +1,15 @@
+const { db } = require('../configs/postgresConfig');
+
+module.exports.authenticateAdmin = async function (req, res, next) {
+    const { userId } = req.user;
+    await db.one('SELECT users_id, users.role_id FROM users JOIN role ON users.role_id = role.role_id WHERE users_id = $1',[userId])
+        .then(res => {
+            if (res.role_id === 1){
+                next()
+            } else {
+                res.status(403).send('У вас нет прав для использования этой функции!')
+            }
+        }).catch(e => {
+            res.status(403).send('У вас нет прав для использования этой функции!')
+        });
+}
