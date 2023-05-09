c6f9b54b4d6b3fa61829f3609738f9a6adac09a8
Александр <vampir666x@gmail.com>
2023-05-09 21:22:02 +0300

    Добавил опции для функции декодирования

--- parser_service/verifyToken.py
+++ parser_service/verifyToken.py
@@ -9,19 +9,27 @@ load_dotenv()
 JWT_SECRET = os.getenv('JWT_SECRET')
 
 
-async def verify_token(authorization: Optional[str] = Header(None)) -> int:
+async def verify_token(authorization: Optional[str]=Header(None)) -> int:
+    jwt_options = {
+        'verify_signature': False,
+        'verify_exp': True,
+        'verify_nbf': False,
+        'verify_iat': True,
+        'verify_aud': False
+    }
+
     if not authorization:
         raise HTTPException(status_code=401, detail="Токен не найден")
 
-    token_type, token = authorization.split()
+    token_type, token = authorization.scheme, authorization.credentials
     if token_type.lower() != "bearer":
         raise HTTPException(status_code=401, detail="Тип токена должен быть Bearer")
-
     try:
-        payload = jwt.decode(token, JWT_SECRET, algorithms=["HS256"])
-        user_id = payload.get("user_id")
+        payload = jwt.decode(token, JWT_SECRET, algorithms=["HS256"], options=jwt_options)
+        user_id = payload.get("userId")
         if not user_id:
             raise HTTPException(status_code=401, detail="Неверный токен")
         return user_id
-    except jwt.InvalidTokenError:
-        raise HTTPException(status_code=401, detail="Неверный токен")
+    except jwt.PyJWTError as e:
+        print(f"{e}")
+        raise HTTPException(status_code=401, detail=f"{e}")
