3743c1532bbebf89981445bc642b952a1392b396
Александр <vampir666x@gmail.com>
2023-05-09 19:51:51 +0300

    Добавил роли пользователя

--- admin_service/routes/admin.js
+++ admin_service/routes/admin.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/adminController')
 const { authenticateToken } = require('../middleware/authenticateToken')
+const { authenticateAdmin } = require('../middleware/authenticateAdmin')
 
-router.get('/get_users', authenticateToken , controller.getUsers)
+router.get('/get_users', authenticateToken ,authenticateAdmin, controller.getUsers)
 
-router.post('/update_user', authenticateToken, controller.updateUser);
+router.post('/update_user', authenticateToken, authenticateAdmin, controller.updateUser);
 
 
 module.exports = router
