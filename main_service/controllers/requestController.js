29b643d4d88a3082bafe7fcee4957a04b8632147
FzeroH <vampir666x@gmail.com>
2023-05-21 19:28:46 +0300

    Перевел функции книг в отдельный контроллер

--- main_service/controllers/requestController.js
+++ main_service/controllers/requestController.js
@@ -1,25 +1,13 @@
 const { db } = require('../configs/postgresConfig')
 
-module.exports.getPublications = async function (req,res) {
-  try {
-      const result = await db.many('SELECT publication_author, publication_title, publication_year, publication_cost, company_name FROM publication JOIN company ON publication.company_id = company.company_id;')
-      return res.status(200).json(result)
-  } catch (e) {
-      console.error(e)
-      res.status(500).json({
-          message: "Произошла ошибка"
-      })
-  }
-};
-
 module.exports.createRequest = async function(req,res) {
     const { pub_type_id, publication_id, users_id, request_status_id, request_count } = req.body
     try {
         await db.tx(async (transaction) => {
             await transaction.none(`
         WITH pub_req AS (
-          INSERT INTO publication_request(pub_type_id, publication_id, finaly_request_id, users_id, request_status_id, request_count)
-          VALUES ($1, $2, $3, $4, $5, $6)
+          INSERT INTO publication_request(pub_type_id, publication_id, finaly_request_id, users_id, request_status_id, 0)
+          VALUES ($1, $2, $3, $4, $5)
           RETURNING publication_request_id
         )
         INSERT INTO pub_req_students_discipline(students_disciplne_id, request_id)
@@ -35,12 +23,21 @@ module.exports.createRequest = async function(req,res) {
             message: "Произошла ошибка"
         })
     }
-}
+} // Функция не готова к использованию
 
-module.exports.colleteRequest = async function(req,res) {
+module.exports.updateRequestStatus = async function(req,res) {
+    const { request_id, request_status_id} = req.body
     try {
-
+        const result = await db.none(
+            `UPDATE publication_request SET request_status_id = $2 
+                   WHERE request_id = $1`,[request_id, request_status_id])
+        res.status(200).json({
+            message:"Успешно"
+        })
     } catch (e) {
-
+        console.error(e)
+        res.status(500).json({
+            message: "Произошла ошибка"
+        })
     }
 }
