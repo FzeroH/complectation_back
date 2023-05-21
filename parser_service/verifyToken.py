8014c061ba5617d6f6c09e283fb22b5d6e1cd410
FzeroH <vampir666x@gmail.com>
2023-05-21 19:34:11 +0300

    Убрал закомментированный код

--- parser_service/verifyToken.py
+++ parser_service/verifyToken.py
@@ -1,42 +1,3 @@
-# import os
-#
-# from fastapi import Header, HTTPException
-# from typing import Optional
-# from dotenv import load_dotenv
-# from jose import jwt, JWTError
-#
-# load_dotenv()
-# JWT_SECRET = os.getenv('JWT_SECRET')
-#
-#
-# async def verify_token(authorization: Optional[str]=Header(None)) -> int:
-#     jwt_options = {
-#         'verify_signature': True,
-#         'verify_exp': True,
-#         'verify_nbf': False,
-#         'verify_iat': True,
-#         'verify_aud': False
-#     }
-#
-#     if not authorization:
-#         raise HTTPException(status_code=401, detail="Токен не найден")
-#
-#     token_type, token = authorization.scheme, authorization.credentials
-#     print(token)
-#     if token_type.lower() != "bearer":
-#         raise HTTPException(status_code=401, detail="Тип токена должен быть Bearer")
-#
-#     try:
-#         payload = jwt.decode(token, JWT_SECRET, algorithms=["HS256"], options=jwt_options)
-#         print(payload)
-#         user_id = payload.get("userId")
-#         if not user_id:
-#             raise HTTPException(status_code=401, detail="Неверный токен")
-#         return user_id
-#     except JWTError as e:
-#         print(f'{e}')
-#         raise HTTPException(status_code=401, detail=str(e))
-
 import os
 
 from fastapi import Header, HTTPException
@@ -49,7 +10,7 @@ JWT_SECRET = os.getenv('JWT_SECRET')
 JWT_ALG = os.getenv('JWT_ALG')
 
 
-async def verify_token(authorization: Optional[str]=Header(None)) -> int:
+async def verify_token(authorization: Optional[str] = Header(None)) -> int:
     jwt_options = {
         'verify_signature': False,
         'verify_exp': True,
