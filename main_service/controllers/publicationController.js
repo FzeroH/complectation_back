778e1fcd99ee39bf405c7c17efd5667c4222c83d
FzeroH <vampir666x@gmail.com>
2023-05-28 16:02:49 +0300

    Добавил фильтр публикаций по году

--- main_service/controllers/publicationController.js
+++ main_service/controllers/publicationController.js
@@ -4,7 +4,7 @@ module.exports.getPublications = async function (req, res) {
   try {
       const result = await db.many(`
         SELECT publication_id as id, publication_author, publication_title, publication_year, publication_cost, company_name 
-        FROM publication JOIN company ON publication.company_id = company.company_id;`)
+        FROM publication JOIN company ON publication.company_id = company.company_id ORDER BY publication_id;`)
       return res.status(200).json(result)
   } catch (e) {
       console.error(e)
@@ -30,7 +30,23 @@ module.exports.getPublicationById = async function (req, res) {
 
 module.exports.getPublicationType = async function (req, res) {
     try {
-        const result = await db.many(`SELECT pub_type_id, pub_type_name FROM publication_type `)
+        const result = await db.many(`SELECT pub_type_id as value, pub_type_name as title FROM publication_type `)
+        return res.status(200).json(result)
+    } catch (e) {
+        console.error(e)
+        res.status(500).json({
+            message: "Произошла ошибка"
+        })
+    }
+};
+
+module.exports.getFilteredPublications = async function (req, res) {
+    const { direction, field } = req.query
+    try {
+        const result = await db.many(` 
+                SELECT publication_id as id, publication_author, publication_title, publication_year, publication_cost, company_name
+                FROM publication JOIN company ON publication.company_id = company.company_id;
+                ORDER BY publication_year $1;`, [direction, field])
         return res.status(200).json(result)
     } catch (e) {
         console.error(e)
