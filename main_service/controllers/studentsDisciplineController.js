3d3cc9025e9b5aa368f1c224bdc62da8227009a5
FzeroH <vampir666x@gmail.com>
2023-05-28 16:05:04 +0300

    Добавил алиасы для корректного отображения на фронте

--- main_service/controllers/studentsDisciplineController.js
+++ main_service/controllers/studentsDisciplineController.js
@@ -3,7 +3,7 @@ const { db } = require("../configs/postgresConfig");
 module.exports.getDiscipline = async function (req,res) {
     try {
         const result = await db.many(`
-        SELECT sd.discipline_id, discipline_name 
+        SELECT sd.discipline_id as value, discipline_name as title
         FROM students_discipline as sd join discipline as ds on sd.discipline_id = ds.discipline_id
         `)
         return res.status(200).json(result)
@@ -17,9 +17,10 @@ module.exports.getDiscipline = async function (req,res) {
 
 module.exports.getStudentGroupsByDiscipline = async function (req,res) {
     const { id } = req.query
+    console.log("Отработало")
     try {
-        const result = await db.one(`
-            SELECT sd.students_group_id, students_group_name
+        const result = await db.many(`
+            SELECT sd.students_group_id as value, students_group_name as title
             FROM students_discipline as sd join students_group as sg on sd.students_group_id = sg.students_group_id
             join students_group_type as sgt on sg.students_group_type_id = sgt.students_group_type_id where sd.discipline_id = $1
         `, [id])
@@ -33,15 +34,19 @@ module.exports.getStudentGroupsByDiscipline = async function (req,res) {
 };
 
 module.exports.getGroupInfo = async function (req,res) {
-    const { discipline_id,students_group_id } = req.query
+    const { discipline_id,student_group_id } = req.query
     try {
         const result = await db.one(`
-            SELECT students_discipline_semester, students_group_type_name, students_group_count
-            FROM students_discipline as sd
-            join students_group as sg on sd.students_group_id = sg.students_group_id
-            join students_group_type as sgt on sg.students_group_type_id = sgt.students_group_type_id
-            where sd.discipline_id = $1 and sd.students_group_id = $2
-        `, [discipline_id, students_group_id])
+            SELECT ceil(students_group_count / 4) as request_count,
+                   students_discipline_semester,
+                   students_group_type_name as students_group_type
+            FROM public.students_discipline as sd
+                     join students_group as sg on sd.students_group_id = sg.students_group_id
+                     join students_group_type as sgt on sg.students_group_type_id = sgt.students_group_type_id
+            where sd.students_group_id = $2
+              and sd.discipline_id = $1;
+        `, [discipline_id, student_group_id])
+        console.log(result)
         return res.status(200).json(result)
     } catch (e) {
         console.error(e)
