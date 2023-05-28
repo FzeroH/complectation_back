9610f5dcf40912f16c18e9c7a88ed802e5f5b07b
FzeroH <vampir666x@gmail.com>
2023-05-28 20:37:33 +0300

    Добавил алиасы для корректной отправки на фронт

--- main_service/controllers/companyController.js
+++ main_service/controllers/companyController.js
@@ -2,7 +2,7 @@ const { db } = require("../configs/postgresConfig");
 
 module.exports.getCompanyName = async function (req, res){
     try {
-        const result = await db.many(`SELECT company_id, company_name FROM publication_type `)
+        const result = await db.many(`SELECT company_id as value, company_name as title FROM company `)
         return res.status(200).json(result)
     } catch (e) {
         console.error(e)
