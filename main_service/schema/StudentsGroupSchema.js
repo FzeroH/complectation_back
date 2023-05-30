407b506629799f9e8ec9bf500d02388c0658cd79
FzeroH <vampir666x@gmail.com>
2023-05-30 21:37:06 +0300

    Изменил базовую схему таблицы

--- main_service/schema/StudentsGroupSchema.js
+++ main_service/schema/StudentsGroupSchema.js
@@ -8,16 +8,6 @@ const StudentsGroupSchema = {
             disabled: true
         },
         {
-            title: 'Кафедра',
-            name:'cafedra_name',
-            type: 'string'
-        },
-        {
-            title: 'Тип группы',
-            name:'students_group_type_name',
-            type: 'string'
-        },
-        {
             title: 'Группа',
             name:'students_group_name',
             type: 'string'
