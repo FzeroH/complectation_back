0a4d153d5ab7eb8c2fbaed360be2e447820cc73f
FzeroH <vampir666x@gmail.com>
2023-06-01 22:13:11 +0300

    Изменил названия эндпоинтов

--- main_service/routes/request.js
+++ main_service/routes/request.js
@@ -10,12 +10,10 @@ router.put('/change_status', controller.changeRequestStatus);
 
 router.post('/create_order', controller.createOrder);
 
-router.get('/requests', controller.getRequests);
+router.get('/all_requests', controller.getRequests);
 
-router.get('/requests_by_id', controller.getRequestsByUserId);
+router.get('/requests', controller.getRequestsByUserId);
 
 router.get('/get_filtered_request', controller.getFilteredRequest);
 
 module.exports = router
-
-//TODO: Сделать функцию получения списка всех заказов
