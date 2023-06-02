4e238e8055bd30f02162dc8a37df959aab10e357
FzeroH <vampir666x@gmail.com>
2023-06-02 17:48:19 +0300

    Отредактировал путь до статических файлов

--- main_service/app.js
+++ main_service/app.js
@@ -30,7 +30,7 @@ app.use(cors({
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/auth',authRoutes);
 app.use('/api/admin',adminRoutes);
