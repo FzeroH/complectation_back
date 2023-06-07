c838cbc1934cb40805351bb14331e11a4ef2295f
FzeroH <vampir666x@gmail.com>
2023-06-07 07:09:51 +0300

    Добавил сессии express. ID пользователя теперь хранится в сессии

--- proxy_service/controllers/authController.js
+++ proxy_service/controllers/authController.js
@@ -4,6 +4,8 @@ module.exports.login = async function (req, res) {
     try {
         const { body } = req;
         const result = await proxy_main.post('/api/auth/login', body);
+        req.session.users_id = result.data.users_id
+        console.log(req.session.users_id)
         res.status(200).json(result.data);
     } catch (error) {
         res.status(401).json({
