7eb0bd860b811ac845766d62b9c8810a58be2eec
FzeroH <vampir666x@gmail.com>
2023-06-07 07:10:13 +0300

    Добавил сессии express и подключение к redis.

--- proxy_service/app.js
+++ proxy_service/app.js
@@ -7,6 +7,18 @@ require('dotenv').config();
 const bodyParser = require('body-parser');
 const fs = require('fs');
 
+
+const session = require('express-session')
+const redisStorage = require('connect-redis')(session);
+const redis = require('redis');
+const client = redis.createClient();
+
+const host = '45.130.151.194';
+const port = 6379;
+client.on(`error`,err=>{
+    console.log(err)
+});
+
 const app = express();
 // const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
 
@@ -15,6 +27,21 @@ app.use(morgan('dev'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+    store: new redisStorage({
+            host: host,
+            port: 6379,
+            client: client,}),
+    saveUninitialized: true,
+    secret: 'keyboard cat',
+    key: 'sid',
+    cookie: {
+        path:'/',
+        httpOnly: true,
+        maxAge: null
+    },
+    encode: false
+}))
 app.use(cors({
     origin: '*'
 }));
@@ -40,5 +67,9 @@ app.use('/api/', studentsDisciplineRoutes);
 app.use('/api/', usertRoutes);
 app.use('/api/document/', documentRoutes)
 
+app.get('/test',async (req,res)=>{
+    console.log(req.sessionID);
+    res.sendStatus(200);
+})
 
 module.exports = app;
