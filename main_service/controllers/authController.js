73722849d5bc0f4d449182f81e7cc441b4ea0fe1
maksimov_av <maksimov_av@stulipetsk.ru>
2023-06-08 13:33:48 +0300

    Изменил данные, получаемые при авторизации

--- main_service/controllers/authController.js
+++ main_service/controllers/authController.js
@@ -19,13 +19,19 @@ module.exports.login = async function (req, res){
         }
 
         const token = jwt.sign({ userId: user.users_id }, process.env.JWT_SECRET, { expiresIn: '1d' }, { algorithm: 'HS256' });
-
-        const data = await db.one(`SELECT sd.users_id, role_name, u.users_first_name as first_name, u.users_last_name as last_name, users_email,
-            ds.cafedra_id, cf.cafedra_name
+        let data = {}
+        if (user.role_name === 'teacher'){
+            data = await db.one(`SELECT sd.users_id, role_name, u.users_first_name as first_name, u.users_last_name as last_name, users_email,
+             ds.cafedra_id, cf.cafedra_name
             from students_discipline as sd join users as u
             on sd.users_id = u.users_id join role on u.role_id = role.role_id
             join discipline as ds on sd.discipline_id = ds.discipline_id 
             join cafedra as cf on ds.cafedra_id = cf.cafedra_id where sd.users_id = ${user.users_id} limit 1;`,)
+        } else {
+            data = await db.one(`SELECT users_id, users_first_name as first_name,
+                                              users_last_name as last_name, users_email, users_password, role_name
+                                       FROM users JOIN role ON users.role_id = role.role_id WHERE users_id = ${user.users_id};`,)
+        }
         res.status(200).json({
             users_id: data.users_id,
             cafedra_id: data.cafedra_id,
