9563c01dca5980c15080e4bec2c945906d979584
maksimov_av <maksimov_av@stulipetsk.ru>
2023-06-08 14:57:08 +0300

    Изменил запросы для получения дисциплин, групп и данных о группах

--- main_service/controllers/studentsDisciplineController.js
+++ main_service/controllers/studentsDisciplineController.js
@@ -1,11 +1,14 @@
 const { db } = require("../configs/postgresConfig");
 
 module.exports.getDiscipline = async function (req,res) {
+    const {users_id} = req.query;
     try {
         const result = await db.many(`
         SELECT sd.discipline_id as value, discipline_name as title
-        FROM students_discipline as sd join discipline as ds on sd.discipline_id = ds.discipline_id
-        `)
+        FROM students_discipline as sd 
+        join discipline as ds on sd.discipline_id = ds.discipline_id
+        where users_id = $1
+        `, [+users_id])
         return res.status(200).json(result)
     } catch (e) {
         console.error(e)
@@ -16,13 +19,13 @@ module.exports.getDiscipline = async function (req,res) {
 };
 
 module.exports.getStudentGroupsByDiscipline = async function (req,res) {
-    const { id } = req.query
+    const { id, users_id } = req.query
     try {
-        const result = await db.many(`
+        const result = await db.query(`
             SELECT sd.students_group_id as value, students_group_name as title
             FROM students_discipline as sd join students_group as sg on sd.students_group_id = sg.students_group_id
-            join students_group_type as sgt on sg.students_group_type_id = sgt.students_group_type_id where sd.discipline_id = $1
-        `, [id])
+            join students_group_type as sgt on sg.students_group_type_id = sgt.students_group_type_id where sd.discipline_id = $1 and users_id = $2
+        `, [+id, +users_id])
         return res.status(200).json(result)
     } catch (e) {
         console.error(e)
@@ -33,19 +36,20 @@ module.exports.getStudentGroupsByDiscipline = async function (req,res) {
 };
 
 module.exports.getGroupInfo = async function (req,res) {
-    const { discipline_id,student_group_id } = req.query
+    const { discipline_id,student_group_id, users_id } = req.query
     try {
         const result = await db.one(`
             SELECT ceil(students_group_count / 4) as request_count,
                    students_group_count,
                    students_discipline_semester,
+                   students_group_name,
                    students_group_type_name as students_group_type
             FROM public.students_discipline as sd
                      join students_group as sg on sd.students_group_id = sg.students_group_id
                      join students_group_type as sgt on sg.students_group_type_id = sgt.students_group_type_id
             where sd.students_group_id = $2
-              and sd.discipline_id = $1;
-        `, [discipline_id, student_group_id])
+              and sd.discipline_id = $1 and users_id = $3; 
+        `, [discipline_id, student_group_id, users_id])// TODO: Изменить запрос, чтобы происходила группировка данных о группе.
         return res.status(200).json(result)
     } catch (e) {
         console.error(e)
--- proxy_service/controllers/studentsDisciplineController.js
+++ proxy_service/controllers/studentsDisciplineController.js
@@ -1,8 +1,9 @@
 const { proxy_main } = require('../config/axios.config');
 
 module.exports.getDiscipline = async function (req, res) {
+    const { users_id } = req.session.user
     try {
-        const response = await proxy_main.get('/api/discipline');
+        const response = await proxy_main.get(`/api/discipline/?users_id=${users_id}`);
         const discipline = response.data;
         res.status(200).json(discipline);
     } catch (error) {
@@ -14,10 +15,11 @@ module.exports.getDiscipline = async function (req, res) {
 }
 
 module.exports.getStudentGroupsByDiscipline = async function (req, res) {
+    const { users_id } = req.session.user
     try {
         const disciplineId = req.query.id;
         console.log(disciplineId)
-        const response = await proxy_main.get(`/api/student_group/?id=${disciplineId}`);
+        const response = await proxy_main.get(`/api/student_group/?id=${disciplineId}&users_id=${users_id}`);
         const studentGroups = response.data;
         res.status(200).json(studentGroups);
     } catch (error) {
@@ -29,9 +31,10 @@ module.exports.getStudentGroupsByDiscipline = async function (req, res) {
 }
 
 module.exports.getGroupInfo = async function (req, res) {
+    const { users_id } = req.session.user
     try {
         const { discipline_id,student_group_id } = req.query;
-        const response = await proxy_main.get(`/api/group_info?discipline_id=${discipline_id}&student_group_id=${student_group_id}`);
+        const response = await proxy_main.get(`/api/group_info?discipline_id=${discipline_id}&student_group_id=${student_group_id}&users_id=${users_id}`);
         const groupInfo = response.data;
         res.status(200).json(groupInfo);
     } catch (error) {
