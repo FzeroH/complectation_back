ed5227311540cc62ab4752e3b8ec75337bda498a
FzeroH <vampir666x@gmail.com>
2023-06-09 16:41:28 +0300

    Изменил http запрос

--- main_service/routes/document.js
+++ main_service/routes/document.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/documentController');
 
-router.post('/download', controller.generateDocument)
+router.get('/download', controller.generateDocument)
 
 module.exports = router
--- proxy_service/routes/document.js
+++ proxy_service/routes/document.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/documentController');
 
-router.post('/download', controller.generateDocument)
+router.get('/download', controller.generateDocument)
 
 router.post('/upload', controller.uploadFile)
 
