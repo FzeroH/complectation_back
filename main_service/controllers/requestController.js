4d7aff8027db75ce137ee7d6e877f5b21fa89e80
FzeroH <vampir666x@gmail.com>
2023-06-09 17:47:46 +0300

    Добавил сортировки заявок по убыванию id

--- main_service/controllers/requestController.js
+++ main_service/controllers/requestController.js
@@ -57,34 +57,35 @@ module.exports.getRequests = async function(req,res) {
 
     try {
         const result = await db.manyOrNone(
-            `SELECT pr.request_id as id, request_status_name as status, cafedra_name,
+            `SELECT pr.request_id AS id, request_status_name AS status, cafedra_name,
                     publication_author, publication_title, company_name, publication_year, pub_type_name,
-                    request_count, publication_cost,
+                    request_count, publication_cost, students_group_name,
                     ARRAY_AGG(
                             JSON_BUILD_OBJECT(
                                     'discipline_name', ds.discipline_name,
                                     'students_group_type_name', sgt.students_group_type_name,
-                                    'students_group_name', sgt.students_group_type_name,
+                                    'students_group_name', sg.students_group_name,
                                     'students_discipline_semester', sd.students_discipline_semester
                                 )
-                        ) as recommend_list
-             FROM publication_request as pr
-                      JOIN publication_type as pt ON pr.pub_type_id = pt.pub_type_id
-                      JOIN request_status as rs ON pr.request_status_id = rs.request_status_id
-                      JOIN publication as pub ON pr.publication_id = pub.publication_id
-                      JOIN company as cp ON pub.company_id = cp.company_id
-                      JOIN users as us ON pr.users_id = us.users_id
-                      JOIN pub_req_students_discipline as prsd ON prsd.request_id = pr.request_id
-                      JOIN students_discipline as sd ON prsd.students_discipline_id = sd.students_discipline_id
-                      JOIN discipline as ds ON sd.discipline_id = ds.discipline_id
-                      JOIN cafedra as cf ON ds.cafedra_id = cf.cafedra_id
-                      JOIN students_group as sg ON sd.students_group_id = sg.students_group_id
-                      JOIN students_group_type as sgt ON sg.students_group_type_id = sgt.students_group_type_id
-             ${company}
-             ${statusCondition}
+                        ) AS recommend_list
+             FROM publication_request AS pr
+                      JOIN publication_type AS pt ON pr.pub_type_id = pt.pub_type_id
+                      JOIN request_status AS rs ON pr.request_status_id = rs.request_status_id
+                      JOIN publication AS pub ON pr.publication_id = pub.publication_id
+                      JOIN company AS cp ON pub.company_id = cp.company_id
+                      JOIN users AS us ON pr.users_id = us.users_id
+                      JOIN pub_req_students_discipline AS prsd ON prsd.request_id = pr.request_id
+                      JOIN students_discipline AS sd ON prsd.students_discipline_id = sd.students_discipline_id
+                      JOIN discipline AS ds ON sd.discipline_id = ds.discipline_id
+                      JOIN cafedra AS cf ON ds.cafedra_id = cf.cafedra_id
+                      JOIN students_group AS sg ON sd.students_group_id = sg.students_group_id
+                      JOIN students_group_type AS sgt ON sg.students_group_type_id = sgt.students_group_type_id
+                 ${company}
+                 ${statusCondition}
              GROUP BY pr.request_id, request_status_name, cafedra_name,
-                      publication_author, publication_title, company_name, publication_year, pub_type_name,
-                      request_count, publication_cost;`)
+                 publication_author, publication_title, company_name, publication_year, pub_type_name,
+                 request_count, publication_cost, sg.students_group_name
+             ORDER BY pr.request_id DESC;`)
         res.status(200).json(result)
     } catch (e) {
         console.error(e)
@@ -100,12 +101,12 @@ module.exports.getRequestsByUserId = async function(req,res) {
         const result = await db.manyOrNone(
             `SELECT pr.request_id as id, request_status_name as status, cafedra_name,
                     publication_author, publication_title, company_name, publication_year, pub_type_name,
-                    request_count, publication_cost,
+                    request_count, publication_cost, students_group_name,
                     ARRAY_AGG(
                             JSON_BUILD_OBJECT(
                                     'discipline_name', ds.discipline_name,
                                     'students_group_type_name', sgt.students_group_type_name,
-                                    'students_group_name', sgt.students_group_type_name,
+                                    'students_group_name', sg.students_group_name,
                                     'students_discipline_semester', sd.students_discipline_semester
                                 )
                         ) as recommend_list
@@ -124,7 +125,8 @@ module.exports.getRequestsByUserId = async function(req,res) {
              WHERE pr.users_id = $1
              GROUP BY pr.request_id, request_status_name, cafedra_name,
                       publication_author, publication_title, company_name, publication_year, pub_type_name,
-                      request_count, publication_cost;`,
+                      request_count, publication_cost, sg.students_group_name
+             ORDER BY pr.request_id DESC;`,
             [+id]
         );
         res.status(200).json(result)
